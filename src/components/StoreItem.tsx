486132963606bf5118d2ec6b831b94ce1d334c8d
Katarzyna Rachwal <k2.rachwal@gmail.com>
2023-06-23 14:52:09 +0200

    Currency formater

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
-import { Card } from "react-bootstrap"
+import { Card } from "react-bootstrap";
+import { formatCurrency } from "../utilities/formatCurrency";
 
 type StoreItemProps = {
     id: number,
@@ -12,9 +13,10 @@ return (
     <Card>
         <Card.Img variant='top' src={imgUrl} height='200px' style={{objectFit: 'cover'}}/>
         <Card.Body>
-            <Card.Title>
-                {name}
-            </Card.Title>
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+        </Card.Title>
         </Card.Body>
     </Card>
 )
